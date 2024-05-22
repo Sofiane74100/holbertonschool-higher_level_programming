417b0f877d49eecf794d1c725af164f58271857f
Sofiane74100 <arfane.sofiane@gmail.com>
2024-05-22 21:03:30 +0200

    h

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     nb_print = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print(my_list[i], end="")
             nb_print += 1
-            print()
-    except IndexError:
-        print()
-    finally:
-        return nb_print
+        except IndexError:
+            break
+    print()
+    return nb_print
