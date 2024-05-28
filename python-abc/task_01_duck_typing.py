1368671fdb3d9d0f2e23525720a41370f59a3ebc
Sofiane74100 <arfane.sofiane@gmail.com>
2024-05-28 20:19:30 +0200

    n

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -12,8 +12,6 @@ class Shape(ABC):
 
 class Circle(Shape):
     def __init__(self, radius):
-        if radius <= 0:
-            raise ValueError("Radius must be a positive number")
         self.radius = radius
     
     def area(self):
@@ -24,8 +22,6 @@ class Circle(Shape):
 
 class Rectangle(Shape):
     def __init__(self, width, height):
-        if width <= 0 or height <= 0:
-            raise ValueError("Width and height must be positive numbers")
         self.width = width
         self.height = height
     
