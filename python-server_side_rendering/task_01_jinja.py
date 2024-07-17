5aa18b23a38ab684ae5a39a1e932387cbcc65210
Sofiane74100 <arfane.sofiane@gmail.com>
2024-07-17 12:21:55 +0200

    12

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import json
 
 app = Flask(__name__)
 
@@ -14,20 +13,6 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
-
-@app.route('/items')
-def items():
-    # Lire le fichier JSON
-    try:
-        with open('items.json') as file:
-            data = json.load(file)
-            items_list = data.get('items', [])
-    except (FileNotFoundError, json.JSONDecodeError) as e:
-        print(f"Erreur lors de la lecture du fichier JSON : {e}")
-        items_list = []
-
-    # Rendre le template avec les données
-    return render_template('items.html', items=items_list)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
